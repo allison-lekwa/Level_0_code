6da314171a766cde07313c1d21d5333c05e05620
Allison Onyeije <allison2spartamath@gmail.com>
2021-02-26 16:23:18 +0100

    upgrade - this takes four input values

--- task_06.js
+++ task_06.js
@@ -1,12 +1,16 @@
 // This function returns the maximum of three inputed numbers
-function maxNum(num_1,num_2,num_3){
-    if(num_1 > num_2 && num_1 > num_3){
+function maxNum(num_1,num_2,num_3, num_4){
+
+    if(num_1 > num_2 && num_1 > num_3 && num_1 > num_4){
         console.log(num_1);
-    }else if(num_2 > num_1 && num_2 > num_3){
+    }else if(num_2 > num_1 && num_2 > num_3 && num_2 > num_4){
         console.log(num_2);
-    }else{
+    }else if(num_3 > num_1 && num_3 > num_2 && num_3 > num_4){
         console.log(num_3);
+    }
+    else{
+        console.log(num_4);
     }
 }
 // Testing Code
-maxNum(4,7,2)
+maxNum(4,7,2,12)
