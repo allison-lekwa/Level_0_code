10e722ac93cb11187d3cf6f306a8fc14ee7467c8
Allison Onyeije <allison2spartamath@gmail.com>
2021-02-26 17:03:58 +0100

    Added a new task: task_07

---
+++ task_07.js
@@ -0,0 +1,13 @@
+//The function converts temperature from celsius to fahrenheit
+function TempCelsius(fahrenheit){
+    var celsiusValue = (5/9)*(fahrenheit - 32);
+    console.log(celsiusValue);
+}
+
+function TempFahrenheit(celsius){
+    var fahrenheitValue = (5/9)*celsius + 32;
+    console.log(fahrenheitValue);
+}
+
+TempCelsius(32);
+TempFahrenheit(36);
